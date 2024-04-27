e795a65b30cad3a3b23be6b23d9da8e1fcf73159
Zhu, Shenli <davyzhu@users.noreply.github.com>
2024-04-27 14:50:20 +0800

    Update streamlit_app.py
    add llama 3, remove llama 2

--- groq_streamlit_demo/streamlit_app.py
+++ groq_streamlit_demo/streamlit_app.py
@@ -30,12 +30,21 @@ if "selected_model" not in st.session_state:
 
 # Define model details
 models = {
+    "llama3-70b-8192": {
+        "name": "LLaMA3-70b-Instruct",
+        "tokens": 8192,
+        "developer": "Meta",
+    },
+    "llama3-8b-8192": {
+        "name": "LLaMA3-8b-Instruct",
+        "tokens": 8192,
+        "developer": "Meta",
+    },
     "mixtral-8x7b-32768": {
         "name": "Mixtral-8x7b-Instruct-v0.1",
         "tokens": 32768,
         "developer": "Mistral",
     },
-    "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
     "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
 }
 
