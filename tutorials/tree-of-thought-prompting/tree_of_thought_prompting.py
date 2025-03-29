185021cced11e1d4073c9926dba4d9af7a4f3729
Bertan Berker <76100859+MozartofCode@users.noreply.github.com>
2025-03-29 20:40:57 +0000

    Starting the tree of thought task solver example

---
+++ tutorials/tree-of-thought-prompting/tree_of_thought_prompting.py
@@ -0,0 +1,5 @@
+# @ Author: Bertan Berker
+# @ Language: Python 3.11.9
+# This script is an example of a tree of thought implementation for solving a reasoning task
+# Given the prompt of a reasoning task, the script will generate a tree of thought and then
+# solve the task by traversing the tree.
